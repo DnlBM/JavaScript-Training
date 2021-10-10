9e845dd0b60b9612d7f84d7dbc0ee8aaf8958dfb
Daniel <dnloblanco@gmail.com>
2021-10-09 22:38:32 -0500

    Finished Introduction to Arrays and Coding Challenge #2, pushing from VS.Code

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -29,7 +29,7 @@ score 23, 34 and 27.
 */
 
 //Arrow function to calculate the average of the 3 scores.
-const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3
+/*const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3
 
 //Arrow function with a multiple Ternary expressions to calculate the winner.
 const checkWinner = (avgScoreDolph, avgScoreKoal) => (avgScoreDolph >= (avgScoreKoal * 2)) ? console.log(`Dolphins won (${avgScoreDolph} vs ${avgScoreKoal})`) : (avgScoreKoal >= (avgScoreDolph * 2)) ? console.log(`Koalas won (${avgScoreKoal} vs ${avgScoreDolph})`) : console.log("Nobody Won.");
@@ -53,4 +53,78 @@ averageScoreDolphins = calcAverage(85, 54, 41);
 averageScoreKoalas = calcAverage(23, 34, 27);
 
 //Logging the winner.
-checkWinner(averageScoreDolphins, averageScoreKoalas);
+checkWinner(averageScoreDolphins, averageScoreKoalas);*/
+
+/*
+//Exercise - Calculating birth year multiple years inside an array and saving the result
+// in a second array.
+function calcAge(birthYear) {
+    return 2037 - birthYear;
+}
+
+const years = [1990, 1967, 2002, 2010, 2018];
+
+const answer = [];
+
+var index = 0;
+
+years.forEach(element => { answer.push(calcAge(element)) });
+
+console.log(answer);
+*/
+
+//-----------------------------Coding Challenge #2---------------------------------------
+
+/* Steven is  still building his tip calculator, using the same rules as before:
+Tip 15% of the bill if the bill values is between 50 and 300, and if the values is different, the tip is 20%.
+
+1.Write a function called 'calcTip' that takes any bill values as an input and returns the
+corresponding tip, calculated based on the rules above. Use the function type you like the most.
+Test the function using a bill value of 100.
+2.Now lets use Arrays!, create an array 'bills' containing the test data below.
+3.Create an array 'tips' containing the tip value for each bill, calculates from the function
+you created before.
+4.Bonus: Create an array 'total' contining the total values, so the bill + tip.
+
+Test Data: 125, 555, 44;
+*/
+
+//Creating tip calculating function.
+function calcTip(bill) {
+    if (bill >= 50 && bill <= 300) {
+        return bill * 0.15
+    } else {
+        return bill * 0.20;
+    }
+}
+
+//Creating array of bills.
+const bills = [125, 555, 44];
+
+//Creating array of tips to hold the tip value of the bills array.
+const tips = [];
+
+//Filling the tips array with the tip value from every bill.
+bills.forEach(element => { tips.push(calcTip(element)) });
+
+//Creating a calculateTotal function to sum the bills and tips arrays.
+function calculateTotal(bills, tips) {
+    const total = [];
+    for (let index = 0; index < bills.length; index++) {
+        total[index] = bills[index] + tips[index];
+    }
+    return total;
+}
+
+//Creating an array called total, to hold the value of the bill + tip.
+const total = calculateTotal(bills, tips);
+
+//Printing the results to the console.
+console.log("---------------Bills Array-------------");
+console.log(bills);
+
+console.log("---------------Tips Array--------------");
+console.log(tips);
+
+console.log("---------------Total Array-------------");
+console.log(total);
