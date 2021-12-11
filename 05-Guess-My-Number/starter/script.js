1e2c382a7bd9da653d1ea20c001d32b75c8ebf9c
Daniel <dnloblanco@gmail.com>
2021-12-11 16:58:42 -0600

    "Small JS work, eventListeners example"

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,31 @@
 'use strict';
+/*
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = 'DOM Manipulation just like Ive been doing on AEM';
+
+document.querySelector('.message').style.color = 'red';
+
+*/
+
+//Random Number that will be decided on page load.
+let randomNum = 0;
+
+
+//The function returns a random number from 0 to maxInt-1
+function getRandomInt(maxInt) {
+    return Math.floor(Math.random() * maxInt);
+}
+
+//On page load function, defines the value that is to be stored on the random number.
+document.addEventListener("DOMContentLoaded", function () {
+    randomNum = getRandomInt(21);
+});
+
+//Cick event handler. Checks if the value selected by the user is equal to the random number.
+//If it is, 
+document.querySelector('.check').addEventListener('click', function () {
+    console.log(document.querySelector('.guess').value);
+    console.log(randomNum);
+})
+
