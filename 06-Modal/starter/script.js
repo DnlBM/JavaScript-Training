c8600b942146d6d030076435d784d7ef37d15cff
Daniel Blanco <64988654+DnlBM@users.noreply.github.com>
2022-04-11 09:55:12 -0500

    Added modal open and close functionality.

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnClose = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+//Removes modal and overlay
+function closeModal() {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+//Adds modal and overlay
+const openModal = function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+//To loop through properties is -> for (variable in object).
+//In this case to loop through elements is for (element of NodeList)
+for (let node of btnsOpenModal) {
+    node.addEventListener('click', openModal);
+}
+
+btnClose.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
