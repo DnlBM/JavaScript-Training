c9b046785c67ea863eacebefe905c06fd3798826
Daniel Blanco <64988654+DnlBM@users.noreply.github.com>
2022-04-14 09:46:09 -0500

    Added js scope testing.

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+function calcAge(birthYear) {
+    const age = 2037 - birthYear;
+
+    const printAge = function () {
+        const output = `You are ${age}, born in ${birthYear}`;
+        console.log(output);
+    }
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+        var millenial = true;
+        const str = `Oh, and you're a millenial, ${firstName}`;
+        console.log(str);
+    }
+    console.log(millenial);
+    //console.log(str); Will generate an error.
+    printAge();
+    return age;
+}
+
+const firstName = 'Jonas';
+calcAge(1991);
